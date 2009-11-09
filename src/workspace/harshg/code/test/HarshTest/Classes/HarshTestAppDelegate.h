9d5c769518095d04aa070b52f0483be2674c9fc9
harshg031 <harshg031@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-09 05:16:22 +0000

    Test app for practice

---
+++ src/workspace/harshg/code/test/HarshTest/Classes/HarshTestAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HarshTestAppDelegate.h
+//  HarshTest
+//
+//  Created by Goel, Harsh on 11/2/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HarshTestAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
