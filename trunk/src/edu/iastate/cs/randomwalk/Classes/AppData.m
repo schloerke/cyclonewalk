ba98cc21e448ca67aee5c207f25064d32070fa1f
Schloerke <Schloerke@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-10 23:17:33 +0000

    added the project to a common directory.  Made a view for the main screen

---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/AppData.m
@@ -0,0 +1,14 @@
+//
+//  AppData.m
+//  randomwalk
+//
+//  Created by Barret Schloerke on 11/10/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppData.h"
+
+
+@implementation AppData
+
+@end
---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/WalkData.m
@@ -0,0 +1,14 @@
+//
+//  WalkData.m
+//  randomwalk
+//
+//  Created by Barret Schloerke on 11/10/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "WalkData.h"
+
+
+@implementation WalkData
+
+@end
