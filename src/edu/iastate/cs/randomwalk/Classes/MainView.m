16d1bc0d04995c2523b7b358176eda5fadc6f6ba
emanowusu <emanowusu@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-21 22:39:44 +0000

    updating MainView with walk list population methods

--- src/edu/iastate/cs/randomwalk/Classes/MainView.m
+++ src/edu/iastate/cs/randomwalk/Classes/MainView.m
@@ -9,16 +9,93 @@
 #import "MainView.h"
 
 
+
 @implementation MainView
 
 
--(void) viewWillAppear:(BOOL)animated
-{
-	NSLog(@"F: viewWillAppear MainView");
-	self.navigationController.navigationBarHidden = YES;
+/**
+ * Updates view and model upon item press to the walk table
+ */
+- (void)tableView:(UITableView *)theTableView 
+		didSelectRowAtIndexPath:(NSIndexPath *)newIndexPath {
 	
+    [theTableView deselectRowAtIndexPath:[theTableView indexPathForSelectedRow] animated:NO];
+    UITableViewCell *cell = [theTableView cellForRowAtIndexPath:newIndexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        // update view with walk selection
+		cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+		// update model with walk selection
+		//TODO
+		
+    } else if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+		
+		// update model with walk deselection
+		//TODO
+    }
 }
 
+/**
+ * Sets up the walk list table in MainView
+ */ 
+- (void)viewWillAppear {
+	walkTableView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+	walkTableView.delegate = self;
+	walkTableView.dataSource = self;
+	
+	// calls appropriate methods to set up
+	[walkTableView reloadData];
+}
+
+/**
+ * The walk list table contains two section.
+ * A section for the applications predifined Walks
+ * and a section for user defined walks 
+ */
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+/**
+ * Returns the number of rows per section
+ */
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Number of rows is the number of time zones in the region for the specified section.
+	if(section == 0) { // Application Walks
+		return 5;
+	} else { // User Generated Walks
+		return 5;
+	}
+}
+
+/**
+ * Return the section labels
+ */
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    // The header for the section is the region name -- get this from the region at the section index.
+    //Region *region = [regions objectAtIndex:section];
+    if(section == 0) // Application Walks
+		return @"Random Walks";
+	else // User Generated Walks
+		return @"Favorite Walks";
+}
+
+
+/**
+ * 
+ */
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *MyIdentifier = @"MyIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+   if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:MyIdentifier] autorelease];
+    }
+	
+	// TODO
+    cell.textLabel.text = @"Walk's Name";
+    return cell;
+}
 
 
 /*
@@ -31,12 +108,13 @@
 }
 */
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
-*/
+
+
 
 /*
 // Override to allow orientations other than the default portrait orientation.
@@ -56,6 +134,8 @@
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
+	
+	
 }
 
 
