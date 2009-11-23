bbdac09c62636a94f8baf255e02ba4f904f62d65
harshg031 <harshg031@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-23 11:52:19 +0000

    Added Accelerometer Service to support acceleration. The service shall return the tilts of the iPhone using the in-built accelerometer.

---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/Accelerometer.h
@@ -0,0 +1,36 @@
+//
+//  Accelerometer.h
+//  randomwalk
+//
+//  Created by Harsh Goel on 11/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Accelerometer : NSObject {
+	double xValue;
+	double yValue;
+	double zValue;
+	
+	UIAccelerometer *accelerometer;
+}
+
+/**
+ * Returns array of depth 3 with the following data: 
+ * forward tilt(degrees) – upwards is 00, sreen on top is 90,  
+ * upsidedown is 180, screen facing down is 270.
+ *
+ * sideways tilt(degrees) – portrait is 0, landscapeRight is 90,   
+ * Upside-down is 180, landscapeLeft is 270. 
+ *
+ * compass direction(degrees) – north is 0, East is 90, South is   
+ * 180, West is 270. 
+ */
+- (NSArray*)getOrientation;
+
+-(void) init;
+
+@end
+
