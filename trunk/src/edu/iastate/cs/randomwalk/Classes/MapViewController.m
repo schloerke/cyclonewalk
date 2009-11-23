fddabf903315aa5bfc9f221a8dbd972d3cc2de8e
harshg031 <harshg031@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-23 11:54:21 +0000

    Controllers for Map View. These implement a simple Map which can be initialized with a walk. The controller puts pins on the map to locate the nodes in the walk.

--- trunk/src/edu/iastate/cs/randomwalk/Classes/MapViewController.m
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/MapViewController.m
@@ -1,65 +1,119 @@
-//
-//  MapViewController.m
-//  randomwalk
-//
-//  Created by Harsh Goel on 11/22/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
-
-#import "MapViewController.h"
+#import "MainViewController.h"
+#import "MainView.h"
 
 
 @implementation MapViewController
 
--(id)initWithNibName: (NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
-	if(self = [super initWithNibName:nibNameOrNil bundle: nibBundleOrNil]){
-		//Custom Initialization
+
+-(id)initWithWalk: (WalkData*) walkData{
+	[self showNodes: walkData];
+}
+	 
+/**
+ * Adds nodes to the MapView from the provided Walk
+ */
+-(void)showNodes: (WalkData*) walkData{
+	NSMutableArray *nodeList = [walkData nodeList];
+	//Add nodes to the mPlacemark array
+	for (int i=0; i<[nodeList count]; i++) {
+		CLLocationCoordinate2D tempLocation=mapView.userLocation.coordinate;
+		tempLocation.latitude = [nodeList latitude];
+		tempLocation.longitude = [nodeList longitude];
+		PlaceMark *temp = [[PlaceMark alloc] initWithCoordinate:tempLocation];
+		[mPlacemark addObject:temp]; 
 	}
-	return self;
+	[mapView addAnnotations:mPlacemark];
 }
 
-//Implement viewDid load to do additional setup
--(void)viewDidLoad {
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
 	[super viewDidLoad];
-	
-	mapView=[[MKMapView alloc] initWithFrame:self.view.bounds];
-	mapView.showUserLocation = TRUE;
-	mapView.userLocationVisible = TRUE;
-	mapView.mapType=MKMapTypeStandard;
+	mapView=[[MKMapView alloc] initWithFrame:self.view.frame];
+	mapView.showsUserLocation=TRUE;
 	mapView.delegate=self;
+	[self.view insertSubview:mapView atIndex:0];
+		
+	CLLocationManager *locationManager=[[CLLocationManager alloc] init];
+	locationManager.delegate=self;
+	locationManager.desiredAccuracy=kCLLocationAccuracyNearestTenMeters;
 	
-	/*Region and Zoom*/
+	[locationManager startUpdatingLocation];
+	
+}
+
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+//GeoCoder delegate functions
+
+- (void)reverseGeocoder:(MKReverseGeocoder *)geocoder didFailWithError:(NSError *)error{
+}
+
+- (void)reverseGeocoder:(MKReverseGeocoder *)geocoder didFindPlacemark:(MKPlacemark *)placemark{
+	NSLog(@"Geocoder completed");
+	//Do something
+}
+
+//MapView delegate functions
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation{
+	NSLog(@"View for Annotation is called");
+	MKPinAnnotationView *test=[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"parkingloc"];
+	test.userInteractionEnabled=TRUE;
+	[test setPinColor:MKPinAnnotationColorPurple];
+	return test;
+}
+
+//location manager functions
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+	location=newLocation.coordinate;
+	//One location is obtained.. just zoom to that location
+	
 	MKCoordinateRegion region;
+	region.center=location;
+	//Set Zoom level using Span
 	MKCoordinateSpan span;
-	span.longitudeDelta = 0.2;
-	span.latitudeDelta = 0.2;
+	span.latitudeDelta=.005;
+	span.longitudeDelta=.005;
+	region.span=span;
 	
-	CLLocationCoordinate2d location = mapView.userLocation.coordinate;
-	region.center = location;
-	region.span = span;
-	//TODO 
+	[mapView setRegion:region animated:TRUE];
 	
-}	
-/**
- *
- *
- */
--(IBAction)changeType: (id) sender;
+}
 
-/**
- * Initializes the Camera View with the given Walk Data. The data can 
- * be from multiple walks.
- */ 
-- (void) initWithWalk: (WalkData *) walkData;
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+}
 
-/**
- * Controls the click on the touch screen.
- */
-- (IBAction) mapClicked: (id) sender;
 
-/**
- * Displays data points on top of MapKit for the given walkData
- */
-- (void) showNodes: (WalkData *) walkData;
+- (IBAction)showInfo: (MKPlacemark *)sender{
+	MKReverseGeocoder geocoder = [[MKReverseGeocoder alloc] initWithCoordinate:sender.coordinate];
+	geocoder.delegate = self;
+	[geocoder start];
+}
 
+ 
 @end
