c77a788bacf05ff4ee20a93e6ac81f0fc2313c0e
harshg031 <harshg031@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-24 00:05:49 +0000

    Added classes to represent a Pin on a Map. The Pin can be initialized by a Node for easy placement on the MapView.

---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/MapPin.h
@@ -0,0 +1,26 @@
+//
+//  MapPin.h
+//  randomwalk
+//
+//  Created by Harsh Goel on 11/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "NodeData.h"
+
+@interface MapPin : NSObject <MKAnnotation> {
+	CLLocationCoordinate2D coordinate;
+	NodeData *nodeData;
+}
+
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, readonly) NodeData  *nodeData;
+
+-(id) initWithNodeData: (NodeData *) data;
+-(id) initWithCoordinate: (CLLocationCoordinate2D) coord;
+
+
+@end
---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/MapPin.m
@@ -0,0 +1,41 @@
+//
+//  MapPin.m
+//  randomwalk
+//
+//  Created by Harsh Goel on 11/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapPin.h"
+
+
+@implementation MapPin
+
+@synthesize coordinate, nodeData;
+
+-(id) initWithNodeData: (NodeData *) data{
+	self = [super init];
+	if (self!=nil) {
+		nodeData = data;
+		[data retain];
+		coordinate.latitude = data.latitude;
+		coordinate.longitude = data.longitude;
+	}
+	return self;
+}
+
+
+-(id) initWithCoordinate: (CLLocationCoordinate2D) coord{
+	self = [super init];
+	if (self!=nil) {
+		coordinate = coord;
+	}
+	return self;
+}
+
+-(void) dealloc{
+	[nodeData release]; 
+	[super dealloc];
+}
+
+@end
