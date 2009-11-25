ac32ba5330bb681de86166910e866ef2d969b1df
ankitpatel5 <ankitpatel5@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-25 18:26:44 +0000

    Working on the GPS controller

---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/GPS.h
@@ -0,0 +1,16 @@
+//
+//  GPS.h
+//  randomwalk
+//
+//  Created by Patel, Ankit A on 11/25/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GPS : NSObject {
+
+}
+
+@end
