07d12d89230d3135d9134b4f460a219c1f57215d
ankitpatel5 <ankitpatel5@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-25 18:19:29 +0000

    Fixed some build errors

--- trunk/src/edu/iastate/cs/randomwalk/Classes/DeviceData.m
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/DeviceData.m
@@ -8,26 +8,34 @@
 
 #import "DeviceData.h"
 
-// determines if the device a IPhone 3Gs (Calls hasGPS, hasCamera, hasCompass to determine if the device
-//is suitable. They must all return true) 
--(BOOL) isiPhone3Gs;
-
-// Determines if the device has GPS functionality 
--(BOOL) hasGPS;
-
-// Determines if the device has camera functionality 
--(BOOL) hasCame;
-
-// Determines if the device has compass functionality
--(BOOL) hasCompass;
-
-// Gets the version of the iPhone software 
--(NSString *) getSoftwareVersion;
-
-
-
 @implementation DeviceData
 
+// determines if the device a IPhone 3Gs (Calls hasGPS, hasCamera, hasCompass to determine if the device
+//is suitable. They must all return true) 
+-(BOOL) isiPhone3Gs{
+	return NO;
+	
+}
+
+// Determines if the device has GPS functionality 
+-(BOOL) hasGPS{
+	return NO;
+}
+
+// Determines if the device has camera functionality 
+-(BOOL) hasCame{
+	return NO;
+}
+
+// Determines if the device has compass functionality
+-(BOOL) hasCompass{
+	return NO;
+}
+
+// Gets the version of the iPhone software 
+-(NSString *) getSoftwareVersion{
+	return nil;
+}
 
 
 @end
