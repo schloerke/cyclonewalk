21fa4d4b9d17657727e713d67ffce4c5316e5346
ankitpatel5 <ankitpatel5@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-25 20:28:20 +0000

    *finishing up Device data utility *now testing...

--- trunk/src/edu/iastate/cs/randomwalk/Classes/DeviceData.m
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/DeviceData.m
@@ -7,34 +7,77 @@
 //
 
 #import "DeviceData.h"
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 @implementation DeviceData
 
-// determines if the device a IPhone 3Gs (Calls hasGPS, hasCamera, hasCompass to determine if the device
-//is suitable. They must all return true) 
+@synthesize locationManager;
+
+// determines if the device is a IPhone 3Gs
 -(BOOL) isiPhone3Gs{
-	return NO;
+	NSString *version = [self getSoftwareVersion];
+	if([version isEqualToString:@"iPhone2,1"]){		//device code for 3GS is "iPhone2,1"
+		return YES;
+	}else{
+		return NO;
+	}
+	
 }
 
 // Determines if the device has GPS functionality 
 -(BOOL) hasGPS{
-	return NO;
+	return [locationManager locationServicesEnabled];
 }
 
 // Determines if the device has camera functionality 
--(BOOL) hasCame{
-	return NO;
+-(BOOL) hasCamera{
+	return [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera];
 }
 
 // Determines if the device has compass functionality
 -(BOOL) hasCompass{
-	return NO;
+	//only the IPhone 3GS supports a compass, thus check if the device is a 3Gs
+	return [self isiPhone3Gs];
 }
+
 
 // Gets the version of the iPhone software 
+//device codes:
+//iPhone Simulator == i386
+//iPhone == iPhone1,1
+//3G iPhone == iPhone1,2
+//3GS iPhone == iPhone2,1
+//1st Gen iPod == iPod1,1
+//2nd Gen iPod == iPod2,1
 -(NSString *) getSoftwareVersion{
-	return nil;
+	size_t size;
+		
+	// Set 'oldp' parameter to NULL to get the size of the data
+	// returned so we can allocate appropriate amount of space
+	sysctlbyname("hw.machine", NULL, &size, NULL, 0); 
+		
+	// Allocate the space to store name
+	char *name = malloc(size);
+		
+	// Get the platform name
+	sysctlbyname("hw.machine", name, &size, NULL, 0);
+		
+	// Place name into a string
+	NSString *version = [NSString stringWithCString:name];
+		
+	// Done with this
+	free(name);
+	
+	NSLog(@"device: %@", version);
+	
+	return version;
 }
 
+//deallocate memory [memory management]
+-(void) dealloc{
+	[locationManager release];
+	[super dealloc];
+}
 
 @end
