9ec856e15f70605f61bed750729f6822fcfbea31
ankitpatel5 <ankitpatel5@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-26 01:21:58 +0000

    still working on GPS...can't figure out a bug. Something is wrong with getCurrentLocation (I think line 23 is triggering it...) if sum1 gets a chance to look at it go ahead. Also, try uncommenting the function call from RootViewController.m and see if it prints out the coordinates in the console window

--- trunk/src/edu/iastate/cs/randomwalk/Classes/GPS.h
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/GPS.h
@@ -4,13 +4,25 @@
 //
 //  Created by Patel, Ankit A on 11/25/09.
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+
+//Provides access to available location provider (GPS, WIFI triangulation). 
+
 
 #import <Foundation/Foundation.h>
-
+#import <CoreLocation/CoreLocation.h>
 
 @interface GPS : NSObject {
-
 }
+
+//Determines if the GPS is enabled
++(BOOL) hasGPSEnabled;
+
+//Get the curent longitude and latitude position of the device
++(CLLocationCoordinate2D) getCurrentLocation;
+
+//Calculate distance (in meters) from two longitude/latitude coordinates
++(double) getDistance:(CLLocation *) point;
+
+
 
 @end
--- trunk/src/edu/iastate/cs/randomwalk/Classes/GPS.m
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/GPS.m
@@ -7,8 +7,31 @@
 //
 
 #import "GPS.h"
+#import "DeviceData.h"
 
 
 @implementation GPS
+
+//Determines if the GPS is enabled
++(BOOL) hasGPSEnabled{
+	return [DeviceData hasGPS] ;
+}
+ 
+//Get the curent longitude and latitude position of the device
++(CLLocationCoordinate2D) getCurrentLocation{
+	NSLog(@"\n\nCP1\n\n");
+	CLLocationCoordinate2D retVal = [[[CLLocation alloc] init] coordinate];
+	double latitude = retVal.latitude;
+	double longitude = retVal.longitude;
+	NSLog(@"Latitude: %f  Longitude: %f", latitude, longitude);
+	return retVal;
+}
+//Calculate distance (in meters) from two longitude/latitude coordinates
++(double) getDistance:(CLLocation *) point;{
+	CLLocationCoordinate2D currLoc = [self getCurrentLocation];
+	CLLocation *pointCurr = [[CLLocation alloc] initWithLatitude:currLoc.latitude longitude:currLoc.longitude];
+	return [pointCurr getDistanceFrom:point];
+}
+
 
 @end
