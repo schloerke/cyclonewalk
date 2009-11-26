0fab9b2022c8398d8ef868c308faab67db5d816c
emanowusu <emanowusu@b5d42e54-98b6-11de-aa05-a1f2ad4bc971>
2009-11-26 01:35:50 +0000

    Added over the classes for accessing the camera during Camera View. startCamera takes a UIVIew parameter which we will use as a blank sheet for rendering node information. CamerViewOverlay is where we will define this view behavior

---
+++ trunk/src/edu/iastate/cs/randomwalk/Classes/CameraViewOverlay.m
@@ -0,0 +1,33 @@
+//
+//  CameraViewOverlay.m
+//  randomwalk
+//
+//  Created by Owusu, Emmanuel K on 11/25/09.
+//  Copyright 2009 Iowa State University. All rights reserved.
+//
+
+#import "CameraViewOverlay.h"
+
+
+@implementation CameraViewOverlay
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
